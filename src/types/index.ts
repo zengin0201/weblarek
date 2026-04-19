074899aa6060fa623d099a42c00b1eaaa17289a0
Alexey Borodin <aleksejborodin711@gmail.com>
2026-04-19 20:09:40 +0300

    Add payment type and product interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,36 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+export type TPayment = 'card' | 'cash';
+
+
+export interface IProduct {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+}
+
+export interface IBuyer {
+  payment: TPayment
+  email: string;
+  phone: string;
+  address: string;
+} 
+
+export interface IProductList{
+    total:number;
+    items:IProduct[]
+}
+
+export interface IOrder extends IBuyer{
+    total:number;
+    items:string[]
+}
+
+export interface IOrderResult{
+    id:string,
+    total:number
+}
