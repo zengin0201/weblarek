bec8ac7367f4e204a2547e7d96fa2fc3b36a920f
Alexey Borodin <aleksejborodin711@gmail.com>
2026-05-10 15:48:32 +0300

    Update ProductsData.ts

--- src/components/models/ProductsData.ts
+++ src/components/models/ProductsData.ts
@@ -1,12 +1,19 @@
 import { IProduct } from "../../types";
+import { IEvents } from "../base/Events";
 
 export class ProductsData {
     protected _items: IProduct[] = [];
     protected _preview: string | null = null;
-   
+
+    constructor(protected events: IEvents) {}
 
     set items(items: IProduct[]) {
         this._items = items;
+        this.events.emit('items:changed', { items: this._items });
+    }
+
+    get items() {
+        return this._items;
     }
 
     getItem(id: string) {
@@ -15,14 +22,12 @@ export class ProductsData {
 
     set preview(id: string | null) {
         this._preview = id;
-       
+        if (id) {
+            this.events.emit('preview:changed', this.getItem(id));
+        }
     }
 
     get preview() {
         return this._preview;
-    }
-
-    get items() {
-        return this._items;
     }
 }
