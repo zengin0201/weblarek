0ca38fce3bcca8b8b59fb3954a17ed74710413b0
Alexey Borodin <aleksejborodin711@gmail.com>
2026-05-10 15:49:24 +0300

    Update Component.ts

--- src/components/base/Component.ts
+++ src/components/base/Component.ts
@@ -1,14 +1,22 @@
-/**
- * Базовый компонент
- */
 export abstract class Component<T> {
-    protected constructor(protected readonly container: HTMLElement) {
-        // Учитывайте что код в конструкторе исполняется ДО всех объявлений в дочернем классе
+    protected constructor(protected readonly container: HTMLElement) {}
+    toggleClass(element: HTMLElement, className: string, force?: boolean) {
+        element.classList.toggle(className, force);
     }
 
-    // Инструментарий для работы с DOM в дочерних компонентах
+    protected setText(element: HTMLElement, value: unknown) {
+        if (element) {
+            element.textContent = String(value);
+        }
+    }
 
-    // Установить изображение с альтернативным текстом
+    setDisabled(element: HTMLElement, state: boolean) {
+        if (element) {
+            if (state) element.setAttribute('disabled', 'disabled');
+            else element.removeAttribute('disabled');
+        }
+    }
+
     protected setImage(element: HTMLImageElement, src: string, alt?: string) {
         if (element) {
             element.src = src;
@@ -18,7 +26,6 @@ export abstract class Component<T> {
         }
     }
 
-    // Вернуть корневой DOM-элемент
     render(data?: Partial<T>): HTMLElement {
         Object.assign(this as object, data ?? {});
         return this.container;
