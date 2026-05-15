557eb46ebd4f8d7b9a59a3692521ad6077087414
Alexey Borodin <aleksejborodin711@gmail.com>
2026-05-15 18:07:51 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,16 @@ import { OrderForm, ContactsForm } from './components/View/OrderForms';
 import { Success } from './components/View/Success'; 
 import { cloneTemplate, ensureElement } from './utils/utils';
 import { IProduct, IBuyer } from './types';
+
 const events = new EventEmitter();
 const api = new LarekApi(new Api(API_URL));
 const productsModel = new ProductsData(events);
 const basketModel = new BasketData(events);
 const userModel = new UserData(events);
+
 const page = new Page(document.body, events);
 const modal = new Modal(ensureElement<HTMLElement>('#modal-container'), events);
+
 const cardCatalogTemplate = ensureElement<HTMLTemplateElement>('#card-catalog');
 const cardPreviewTemplate = ensureElement<HTMLTemplateElement>('#card-preview');
 const basketTemplate = ensureElement<HTMLTemplateElement>('#basket');
@@ -29,27 +32,27 @@ const orderTemplate = ensureElement<HTMLTemplateElement>('#order');
 const contactsTemplate = ensureElement<HTMLTemplateElement>('#contacts');
 const successTemplate = ensureElement<HTMLTemplateElement>('#success');
 
-
 const basket = new Basket(cloneTemplate(basketTemplate), events);
 const order = new OrderForm(cloneTemplate(orderTemplate), events);
 const contacts = new ContactsForm(cloneTemplate(contactsTemplate), events);
 const success = new Success(cloneTemplate(successTemplate), { onClick: () => modal.close() });
 
+
 const cardPreview = new CardPreview('card', cloneTemplate(cardPreviewTemplate), {
-    onClick: () => {
-        const item = productsModel.preview;
-        if (item) {
-            if (basketModel.contains(item.id)) {
-                basketModel.remove(item.id);
-            } else {
-                basketModel.add(item); 
-            }
-            modal.close();
-        }
-    }
+    onClick: () => events.emit('preview:toggle')
 });
 
-
+events.on('preview:toggle', () => {
+    const item = productsModel.preview;
+    if (item) {
+        if (basketModel.contains(item.id)) {
+            basketModel.remove(item.id);
+        } else {
+            basketModel.add(item); 
+        }
+        modal.close();
+    }
+});
 
 events.on('items:changed', () => {
     page.catalog = productsModel.items.map(item => {
@@ -65,11 +68,9 @@ events.on('items:changed', () => {
     });
 });
 
-
 events.on('card:select', (item: IProduct) => {
     productsModel.preview = item; 
 });
-
 
 events.on('preview:changed', () => {
     const item = productsModel.preview;
@@ -88,13 +89,12 @@ events.on('preview:changed', () => {
     }
 });
 
-
 events.on('basket:changed', () => {
     page.counter = basketModel.count();
     
     basket.items = basketModel.items.map((item, index) => {
         const card = new CardBasket('card', cloneTemplate(cardBasketTemplate), {
-            onClick: () => basketModel.remove(item.id)
+            onClick: () => events.emit('basket:remove', item)
         });
         return card.render({ title: item.title, price: item.price, index: index + 1 });
     });
@@ -102,10 +102,13 @@ events.on('basket:changed', () => {
 });
 
 
+events.on('basket:remove', (item: IProduct) => {
+    basketModel.remove(item.id);
+});
+
 events.on('basket:open', () => {
     modal.render({ content: basket.render() });
 });
-
 
 events.on('order:open', () => {
     modal.render({ content: order.render() });
@@ -115,11 +118,9 @@ events.on('order:submit', () => {
     modal.render({ content: contacts.render() });
 });
 
-
 events.on(/(^order|^contacts)\..*:change/, (data: { field: keyof IBuyer, value: string }) => {
     userModel.setField(data.field, data.value);
 });
-
 
 events.on('buyer:changed', () => {
     const errors = userModel.validate();
@@ -136,7 +137,6 @@ events.on('buyer:changed', () => {
     contacts.valid = !errors.email && !errors.phone;
     contacts.errors = Object.values({phone: errors.phone, email: errors.email}).filter(i => !!i).join('; ');
 });
-
 
 events.on('contacts:submit', () => {
     const orderData = {
@@ -156,10 +156,8 @@ events.on('contacts:submit', () => {
         .catch(console.error);
 });
 
-
 events.on('modal:open', () => { page.locked = true; });
 events.on('modal:close', () => { page.locked = false; });
-
 
 api.getProductList()
     .then(data => {
